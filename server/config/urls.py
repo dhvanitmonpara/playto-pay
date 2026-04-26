e9336ac9815f37e05bc553b63a6254f51f0dff82
dhvanitmonpara <monparadhvanit@gmail.com>
2026-04-26 19:25:28 +0530

    add healthz route

--- server/config/urls.py
+++ server/config/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 
+def healthz(_request):
+    return JsonResponse({"status": "ok"})
+
 urlpatterns = [
+    path("healthz", healthz),
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.payouts.urls')),
 ]
